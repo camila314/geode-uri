55b988f7ab6898388ad2d5027370028f4fbe02ea
camila314 <47485054+camila314@users.noreply.github.com>
2025-01-19 18:08:58 -0600

    use pointer instead

--- include/GeodeURI.hpp
+++ include/GeodeURI.hpp
@@ -13,14 +13,22 @@ public:
     inline std::string const& getURI() const { return uri; }
 };
 
-inline geode::event::v2::EventHandler<URIEvent> handleURI(
+/***
+ * Handles a URI. If an application attempts to open
+ * a URI starting with `geode://`, this will intercept
+ * the event. 
+ * 
+ * Ex. with a handle path of `level`, the URI `geode://level/128
+ * will call the callback with the string `128`.
+ */
+inline geode::event::v2::EventHandler<URIEvent>* handleURI(
     std::string const& handlePath,
     std::function<void(std::string const&)> callback
 ) {
-    auto hand = geode::event::v2::EventHandler<URIEvent>([handlePath](URIEvent* ev) {
+    auto hand = new geode::event::v2::EventHandler<URIEvent>([handlePath](URIEvent* ev) {
         return geode::utils::string::startsWith(ev->getURI(), handlePath + "/");
     });
-    hand.bind([=](URIEvent* ev) {
+    hand->bind([=](URIEvent* ev) {
         callback(ev->getURI().substr(handlePath.size() + 1));
         return geode::ListenerResult::Stop;
     });
